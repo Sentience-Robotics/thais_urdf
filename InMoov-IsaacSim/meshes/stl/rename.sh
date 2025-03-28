fb9fbde64419a4dd06bb9dabca3ec618e4acacdb
Arcod7 <antoine.esman@epitech.eu>
2025-03-28 13:53:49 +0100

    fix(isaacsim): renaming G.stl files

--- InMoov-IsaacSim/meshes/stl/rename.sh
+++ InMoov-IsaacSim/meshes/stl/rename.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Loop over all files named Texture.*.stl
-for file in Texture.*.stl; do
-  # Remove the dot using sed to get the desired format
-  new_file=$(echo "$file" | sed 's/Texture\.\([0-9]\{3\}\)\.stl/Texture\1.stl/')
-  
-  # Rename the file
+# for file in Texture.*.stl; do
+  # new_file=$(echo "$file" | sed 's/Texture\.\([0-9]\{3\}\)\.stl/Texture\1.stl/')
+for file in G-__*.stl; do
+  new_file=$(echo "$file" | sed -E 's/G-__([0-9]+)\.([0-9]{3})\.stl/G\1_\2.stl/')
+
   mv "$file" "$new_file"
 done
 
